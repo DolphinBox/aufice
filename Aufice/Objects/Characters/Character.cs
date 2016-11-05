dc7e2866b709ef3832e5539d4766f656583b5ff5
espidev <aguy867@gmail.com>
2016-11-05 17:51:00 -0400

    Changes

---
+++ Aufice/Objects/Characters/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufice.Objects.Characters
+{
+    interface Character
+    {
+        double getHealth();
+        double getSpeed();
+        Boolean getHostile();
+       
+    }
+}
